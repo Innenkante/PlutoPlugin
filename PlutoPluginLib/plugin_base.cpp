cec9b3553650dee7181b92c2d8b632159586f8cd
Mark <chemnitz@arnsperger.org>
2017-11-14 17:55:58 +0100

    Restructured the tiny bit of code I have

--- PlutoPluginLib/plugin_base.cpp
+++ PlutoPluginLib/plugin_base.cpp
@@ -1,23 +1,6 @@
 #include "stdafx.h"
 #include "plugin_base.h"
 
-
-std::function<void(gentity_t*, team*, char*)> base::plugin_base::g_say_callback_ = nullptr;
-std::function<void(gentity_t*)> base::plugin_base::client_connect_callback_ = nullptr;
-std::function<void(gentity_t*)> base::plugin_base::client_disconnect_callback_ = nullptr;
-std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> base::plugin_base::scr_player_killed_callback_ = nullptr;
-std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> base::plugin_base::scr_player_damaged_callback_ = nullptr;
-
-
-base::plugin_base::g_say_t base::plugin_base::g_say_pointer_ = nullptr;
-base::plugin_base::scr_player_killed_t base::plugin_base::scr_player_killed_pointer_ = nullptr;
-base::plugin_base::scr_player_damaged_t base::plugin_base::scr_player_damaged_pointer_ = nullptr;
-
-base::plugin_base::gscr_get_entity_t base::plugin_base::gscr_get_entity_pointer_ = (gscr_get_entity_t)0x00494EB0;
-base::plugin_base::scr_get_entity_t base::plugin_base::scr_get_entity_pointer_ = (scr_get_entity_t)0x004B18E0;
-
-bool base::plugin_base::exec_ent_thread_hook_set = false;
-
 int function_handle;
 int object_ref;
 int jump_back = 0x004F0289;
@@ -29,7 +12,7 @@ __declspec(naked) void hk_exec_ent_thread_num()
 	__asm pushad;
 	__asm pushfd;
 
-	base::plugin_base::exec_ent_thread_num_solve(function_handle, object_ref);
+	base::internal::exec_ent_thread_num_helper(function_handle, object_ref);
 
 	__asm popfd;
 	__asm popad;
@@ -42,67 +25,67 @@ __declspec(naked) void hk_exec_ent_thread_num()
 	__asm jmp[jump_back];
 }
 
-void base::plugin_base::install_g_say_callback(std::function<void(gentity_t*, team*, char*)> callback)
+void base::install_g_say_callback(std::function<void(gentity_t*, team*, char*)> callback)
 {
-	g_say_callback_ = callback;
-	g_say_pointer_ = (g_say_t)detour_function((BYTE*)0x0047E900, (BYTE*)hk_g_say, 0x6);
+	internal::g_say_callback_ = callback;
+	internal::g_say_pointer_ = (internal::g_say_t)internal::detour_function((BYTE*)0x0047E900, (BYTE*)internal::hk_g_say, 0x6);
 }
 
-void base::plugin_base::install_client_connected_callback(std::function<void(gentity_t*)> callback)
+void base::install_client_connected_callback(std::function<void(gentity_t*)> callback)
 {
-	client_connect_callback_ = callback;
+	internal::client_connect_callback_ = callback;
 	
-	if (!exec_ent_thread_hook_set)
+	if (!internal::exec_ent_thread_hook_set)
 	{
-		detour_function((BYTE*)0x004F0280, (BYTE*)hk_exec_ent_thread_num, 0x6);
-		exec_ent_thread_hook_set = true;
+		internal::detour_function((BYTE*)0x004F0280, (BYTE*)hk_exec_ent_thread_num, 0x6);
+		internal::exec_ent_thread_hook_set = true;
 	}
 
 }
 
-void base::plugin_base::install_client_disconnect_callback(std::function<void(gentity_t*)> callback)
+void base::install_client_disconnect_callback(std::function<void(gentity_t*)> callback)
 {
-	client_disconnect_callback_ = callback;
+	internal::client_disconnect_callback_ = callback;
 
-	if (!exec_ent_thread_hook_set)
+	if (!internal::exec_ent_thread_hook_set)
 	{
-		detour_function((BYTE*)0x004F0280, (BYTE*)hk_exec_ent_thread_num, 0x6);
-		exec_ent_thread_hook_set = true;
+		internal::detour_function((BYTE*)0x004F0280, (BYTE*)hk_exec_ent_thread_num, 0x6);
+		internal::exec_ent_thread_hook_set = true;
 	}
 }
 
-void base::plugin_base::install_scr_player_killed_callback(
+void base::install_scr_player_killed_callback(
 	std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> callback)
 {
-	scr_player_killed_callback_ = callback;
-	scr_player_killed_pointer_ = (scr_player_killed_t)detour_function((BYTE*)0x004AD030, (BYTE*)hk_scr_player_killed, 0x6);
+	internal::scr_player_killed_callback_ = callback;
+	internal::scr_player_killed_pointer_ = (internal::scr_player_killed_t)internal::detour_function((BYTE*)0x004AD030, (BYTE*)internal::hk_scr_player_killed, 0x6);
 }
 
-void base::plugin_base::install_scr_player_damaged_callback(
+void base::install_scr_player_damaged_callback(
 	std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> callback)
 {
-	scr_player_damaged_callback_ = callback;
-	scr_player_damaged_pointer_ = (scr_player_damaged_t)detour_function((BYTE*)0x004ACE50, (BYTE*)hk_scr_player_damaged, 0x6);
+	internal::scr_player_damaged_callback_ = callback;
+	internal::scr_player_damaged_pointer_ = (internal::scr_player_damaged_t)internal::detour_function((BYTE*)0x004ACE50, (BYTE*)internal::hk_scr_player_damaged, 0x6);
 }
 
 
-gentity_t* base::plugin_base::gscr_get_entiy(int entity_number)
+gentity_t* base::gscr_get_entiy(int entity_number)
 {
-	return gscr_get_entity_pointer_(entity_number);
+	return internal::gscr_get_entity_pointer_(entity_number);
 }
 
-gentity_t* base::plugin_base::scr_get_entity(int object_number)
+gentity_t* base::scr_get_entity(int object_number)
 {
-	return scr_get_entity_pointer_(object_number);
+	return internal::scr_get_entity_pointer_(object_number);
 }
 
-char* base::plugin_base::hk_g_say(gentity_t* entity, team team, char* msg)
+char* base::internal::hk_g_say(gentity_t* entity, team team, char* msg)
 {
 	g_say_callback_(entity, &team, msg);
 	return g_say_pointer_(entity, team, msg);
 }
 
-void base::plugin_base::exec_ent_thread_num_solve(int function_handle, int object_ref)
+void base::internal::exec_ent_thread_num_helper(int function_handle, int object_ref)
 {
 	if (function_handle == 412300 && client_connect_callback_ != nullptr)
 		client_connect_callback_(scr_get_entity(object_ref));
@@ -110,7 +93,7 @@ void base::plugin_base::exec_ent_thread_num_solve(int function_handle, int objec
 		client_disconnect_callback_(scr_get_entity(object_ref));
 }
 
-void base::plugin_base::hk_scr_player_killed(gentity_t* player_who_died, gentity_t* inflictor, gentity_t* killer,
+void base::internal::hk_scr_player_killed(gentity_t* player_who_died, gentity_t* inflictor, gentity_t* killer,
 	int damage, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int hit_location, int unkown_0,
 	int unknow_1)
 {
@@ -118,7 +101,7 @@ void base::plugin_base::hk_scr_player_killed(gentity_t* player_who_died, gentity
 	return scr_player_killed_pointer_(player_who_died, inflictor, killer, damage, mod, weapon_index, is_alternate_weapon, direction, hit_location, unkown_0, unknow_1);
 }
 
-void base::plugin_base::hk_scr_player_damaged(gentity_t* player_who_was_damaged, gentity_t* inflictor,
+void base::internal::hk_scr_player_damaged(gentity_t* player_who_was_damaged, gentity_t* inflictor,
 	gentity_t* player_who_damanged, int damage, int unknown_0, int mod, int weapon_index, bool is_alternate_weapon,
 	vec3_t direction, int unknown_1, int hit_location, int unknown_2)
 {
@@ -127,7 +110,7 @@ void base::plugin_base::hk_scr_player_damaged(gentity_t* player_who_was_damaged,
 }
 
 
-void* base::plugin_base::detour_function(BYTE* src, const BYTE* dst, const int len)
+void* base::internal::detour_function(BYTE* src, const BYTE* dst, const int len)
 {
 	BYTE* jmp = (BYTE*)malloc(len + 5);
 	DWORD dwBack;
--- PlutoPluginLib/plugin_base.h
+++ PlutoPluginLib/plugin_base.h
@@ -3,48 +3,47 @@
 #include <functional>
 namespace base
 {
-	class plugin_base
+
+	void install_g_say_callback(std::function<void(gentity_t*, team*, char*)> callback);
+	void install_client_connected_callback(std::function<void(gentity_t*)> callback);
+	void install_client_disconnect_callback(std::function<void(gentity_t*)> callback);
+	void install_scr_player_killed_callback(std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> callback);
+	void install_scr_player_damaged_callback(std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> callback);
+
+	gentity_t* gscr_get_entiy(int entity_number);
+	gentity_t* scr_get_entity(int object_number);
+
+	namespace internal
 	{
-	public:
-		static void install_g_say_callback(std::function<void(gentity_t*, team*, char*)> callback);
-		static void install_client_connected_callback(std::function<void(gentity_t*)> callback);
-		static void install_client_disconnect_callback(std::function<void(gentity_t*)> callback);
-		static void install_scr_player_killed_callback(std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> callback);
-		static void install_scr_player_damaged_callback(std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> callback);
-
-		static gentity_t* gscr_get_entiy(int entity_number);
-		static gentity_t* scr_get_entity(int object_number);
-
-		static void* detour_function(BYTE * src, const BYTE * dst, const int len);
-
-		static void exec_ent_thread_num_solve(int function_handle, int object_ref);
-	private:
-
 		typedef char*(__cdecl* g_say_t)(gentity_t* entity, team team, char* msg);
 		typedef void(__cdecl* scr_player_killed_t)(gentity_t* player_who_died, gentity_t* inflictor, gentity_t* killer, int damage, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int hit_location, int unkown_0, int unknow_1);
 		typedef void(__cdecl* scr_player_damaged_t)(gentity_t* player_who_was_damaged, gentity_t* inflictor, gentity_t* player_who_damanged, int damage, int unknown_0, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int unknown_1, int hit_location, int unknown_2);
 		typedef gentity_t*(__cdecl* gscr_get_entity_t)(int entity_number);
 		typedef gentity_t*(__cdecl* scr_get_entity_t)(int obj_entity_number);
 
-		static g_say_t g_say_pointer_;
-		static scr_player_killed_t scr_player_killed_pointer_;
-		static scr_player_damaged_t scr_player_damaged_pointer_;
-		static scr_get_entity_t scr_get_entity_pointer_;
+		static g_say_t g_say_pointer_ = nullptr;
+		static scr_player_killed_t scr_player_killed_pointer_ = nullptr;
+		static scr_player_damaged_t scr_player_damaged_pointer_ = nullptr;
 
-		static gscr_get_entity_t gscr_get_entity_pointer_;
 
-		static std::function<void(gentity_t*, team*, char*)> g_say_callback_;
-		static std::function<void(gentity_t*)> client_connect_callback_;
-		static std::function<void(gentity_t*)> client_disconnect_callback_;
-		static std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> scr_player_killed_callback_;
-		static std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> scr_player_damaged_callback_;
+		static gscr_get_entity_t gscr_get_entity_pointer_ = (gscr_get_entity_t)0x00494EB0;
+		static scr_get_entity_t scr_get_entity_pointer_ = (scr_get_entity_t)0x004B18E0;
 
-		static char* hk_g_say(gentity_t* entity, team team, char* msg);
+		static std::function<void(gentity_t*, team*, char*)> g_say_callback_ = nullptr;;
+		static std::function<void(gentity_t*)> client_connect_callback_ = nullptr;;
+		static std::function<void(gentity_t*)> client_disconnect_callback_ = nullptr;
+		static std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> scr_player_killed_callback_ = nullptr;
+		static std::function<void(gentity_t*, gentity_t*, gentity_t*, int*, int*, int*, bool*, vec3_t, int*)> scr_player_damaged_callback_ = nullptr;
 
-		static bool exec_ent_thread_hook_set;
 
-		static void hk_scr_player_killed(gentity_t* player_who_died, gentity_t* inflictor, gentity_t* killer, int damage, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int hit_location, int unkown_0, int unknow_1);
-		static void hk_scr_player_damaged(gentity_t* player_who_was_damaged, gentity_t* inflictor, gentity_t* player_who_damanged, int damage, int unknown_0, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int unknown_1, int hit_location, int unknown_2);
+		static bool exec_ent_thread_hook_set = false;
 
-	};
+		void exec_ent_thread_num_helper(int function_handle, int object_ref);
+
+		char* hk_g_say(gentity_t* entity, team team, char* msg);
+		void hk_scr_player_killed(gentity_t* player_who_died, gentity_t* inflictor, gentity_t* killer, int damage, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int hit_location, int unkown_0, int unknow_1);
+		void hk_scr_player_damaged(gentity_t* player_who_was_damaged, gentity_t* inflictor, gentity_t* player_who_damanged, int damage, int unknown_0, int mod, int weapon_index, bool is_alternate_weapon, vec3_t direction, int unknown_1, int hit_location, int unknown_2);
+
+		void* detour_function(BYTE * src, const BYTE * dst, const int len);
+	}
 }
